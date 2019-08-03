5cb6f0ae34c62085efda42f67e9112aa8500fd46
Ari Putra <ariteknologi@gmail.com>
2019-08-04 05:57:26 +0700

    add home screen

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-
+import { Button } from 'native-base'
 class Home extends React.Component {
     render() {
         return (
-          <View>
-            <Text> Home Screen </Text>
+          <View style={ { 
+              justifyContent: 'center', alignContent: 'space-between', 
+              flex : 1, padding : 25}
+          }>
+            <Text style={ { textAlign : 'center'}}> Selamat Datang </Text>
+            <Button style={{justifyContent: 'center', marginTop : 15}} info>
+              <Text style={ { textAlign : 'center', color : 'white'}}> Buat Analisa Baru </Text>
+            </Button>
           </View>
         );
     }
