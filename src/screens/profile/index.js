22fbc6061ce63d6f33ced73cb1a288937844ec07
Ari Putra <ariteknologi@gmail.com>
2019-08-04 22:52:56 +0700

    integrate user detail api to profile screen

--- src/screens/profile/index.js
+++ src/screens/profile/index.js
@@ -1,11 +1,65 @@
 import React from 'react'
 import { Text, AsyncStorage} from 'react-native'
-import { Container, Content, Card, H3, Button } from 'native-base'
+import { Container, Content, Card, H3, Button, Toast } from 'native-base'
 class Index extends React.Component {
-    _signOutAsync = async () => {
-      await AsyncStorage.clear();
-      this.props.navigation.navigate('Auth');
-    };
+    constructor() {
+      super()
+      this.state = {
+        name : "",
+        age : "",
+        address : "",
+        gender : "",
+        email : ""
+      }
+    }
+
+    componentDidMount = async() => {
+      console.log(AsyncStorage.getItem('token'))
+      fetch('http://35.240.135.149/api/user/detail', {
+          method: 'GET',
+          headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+              'Authorization' : `Bearer ${await AsyncStorage.getItem('token')}`
+          }
+      })
+      .then((response) => response.json())
+      .then((responseJson) => {
+          if(!responseJson.success) {
+              if(responseJson.errors) {
+                  responseJson.errors.map( err => {
+                      Toast.show({
+                          text: err,
+                          buttonText: 'Ok',
+                          type : 'danger'
+                      })
+                  })
+              } else {
+                  Toast.show({
+                      text: responseJson.message,
+                      buttonText: 'Ok',
+                      type : 'danger'
+                  })
+              }
+          } else {
+              this.setState({
+                name : responseJson.data.name,
+                age : responseJson.data.age,
+                gender : responseJson.data.gender,
+                address : responseJson.data.address,
+                email : responseJson.data.email
+              })
+          }
+          console.log(this.state)
+      })
+      .catch((error) => {
+          Toast.show({
+              text: 'Call Administrator',
+              buttonText: 'Ok',
+              type : 'danger'
+          })
+      });
+    }
 
     render() {
         return (
@@ -13,15 +67,15 @@ class Index extends React.Component {
             <Content style={{ padding : 25}}>
                 <Card style={{ padding : 15 }}>
                   <H3> Nama </H3>
-                  <Text style={{ marginBottom : 15}}> Ari Putra </Text>
+                  <Text style={{ marginBottom : 15}}> {this.state.name} </Text>
                   <H3> Umur </H3>
-                  <Text style={{ marginBottom : 15}}> 48 </Text>
+                  <Text style={{ marginBottom : 15}}> {this.state.age} </Text>
                   <H3> Alamat </H3>
-                  <Text style={{ marginBottom : 15}}> Botania </Text>
+                  <Text style={{ marginBottom : 15}}> {this.state.address} </Text>
                   <H3> Jenis Kelamin </H3>
-                  <Text style={{ marginBottom : 15}}> L </Text>
+                  <Text style={{ marginBottom : 15}}> {this.state.gender} </Text>
                   <H3> Email </H3>
-                  <Text style={{ marginBottom : 15}}> ariteknologi@gmail.com </Text>
+                  <Text style={{ marginBottom : 15}}> {this.state.email} </Text>
                   <Button style={{ backgroundColor : '#3F51B5', justifyContent : 'center', alignContent : 'center', marginBottom : 15}}>
                     <Text style={{ color : 'white' }}> Ubah Data </Text>
                   </Button>
